6c7a1a7916070fd831640afbcd2dad5d0e345570
AmrElshaer <amrelsher07@gmail.com>
2020-12-24 12:54:33 +0200

    clean

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { CisStudentRoutingModule } from './CisStudentCrud/cis-student.routing';
 import { StudentPageComponent } from './student-page/student-page.component';
 import { AuthoGurd } from './AuthoGurd';
 import { RegisterationComponent } from './CisStudentCrud/registeration/registeration.component';
-
 import { ProfileComponent } from './profile/profile.component';
 import { CisStudentPostsComponent } from './cis-student-posts/cis-student-posts.component';
 import { CommentServces } from './CommentServse';
@@ -13,9 +12,7 @@ import { AllFriendsComponent } from './all-friends/all-friends.component';
 import { PrivatePostsComponent } from './private-posts/private-posts.component';
 import { TrainingComponent } from './training/training.component';
 import { JobsComponent } from './jobs/jobs.component';
-
 const routes: Routes = [{path:'',redirectTo:'Register',pathMatch:'full'},{path:'Jobs/:Id',component:JobsComponent},{path:'Training/:Id',component:TrainingComponent},{path:'PrivatePosts/:Id',component:PrivatePostsComponent},{path:'AllFrieds/:Id',component:AllFriendsComponent},{path:'HomePage/:Id/:AsVisited/:currentstudent',component:StudentPageComponent,canActivate:[AuthoGurd]},{path:'Publicpost/:Id',component:PublicPostComponent},{path:'AddPosts/:Id',component:CisStudentPostsComponent},{path:'Updateprofile/:Id',component:ProfileComponent},{path:'AddProfile/:Id',component:ProfileComponent},{path:'HomePage/:Id',component:StudentPageComponent,canActivate:[AuthoGurd]},{path:'Register',component:RegisterationComponent}];
-
 @NgModule({
   imports: [CisStudentRoutingModule,RouterModule.forRoot(routes)],
   exports: [RouterModule],
